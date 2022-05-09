c6700e3b5eef9cc9163f1b7db0b36e0e8ee51bf7
Akash Yadav <itsaky01@gmail.com>
2022-05-09 09:26:53 +0530

    Converted import orderer and 'remove unused imports' actions to Kotlin Removed hardcoded action titles in Java code actions

---
+++ lsp/java/src/main/java/com/itsaky/lsp/java/actions/common/RemoveUnusedImportsAction.kt
@@ -0,0 +1,56 @@
+package com.itsaky.lsp.java.actions.common
+
+import com.google.googlejavaformat.java.FormatterException
+import com.google.googlejavaformat.java.RemoveUnusedImports
+import com.itsaky.androidide.actions.ActionData
+import com.itsaky.androidide.utils.ILogger
+import com.itsaky.lsp.java.R.string
+import com.itsaky.lsp.java.actions.BaseCodeAction
+import io.github.rosemoe.sora.widget.CodeEditor
+
+class RemoveUnusedImportsAction : BaseCodeAction() {
+
+    private val log = ILogger.newInstance(javaClass.simpleName)
+    override val id: String = "lsp_java_remove_unused_imports"
+    override var label: String = ""
+    override val titleTextRes: Int = string.action_remove_unused_imports
+
+    override fun prepare(data: ActionData) {
+        super.prepare(data)
+        if (!visible) {
+            return
+        }
+
+        if (!hasRequiredData(data, CodeEditor::class.java)) {
+            markInvisible()
+            return
+        }
+
+        visible = true
+        enabled = true
+    }
+
+    override fun execAction(data: ActionData): Any {
+        val start = System.currentTimeMillis()
+        return try {
+            val editor = requireEditor(data)
+            val content = editor.text
+            val output = RemoveUnusedImports.removeUnusedImports(content.toString())
+            log.info("Removed unused imports in", "${System.currentTimeMillis() - start}ms")
+            output
+        } catch (e: FormatterException) {
+            log.error("Failed to remove unused imports", e)
+            false
+        }
+    }
+
+    override fun postExec(data: ActionData, result: Any) {
+        super.postExec(data, result)
+        if (result is String) {
+            if (result.isNotEmpty()) {
+                val editor = requireEditor(data)
+                editor.setText(result)
+            }
+        }
+    }
+}
