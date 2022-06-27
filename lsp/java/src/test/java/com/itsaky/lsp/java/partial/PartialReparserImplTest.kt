ddf224a9a845761d045e7c34456897d611ec638d
Akash Yadav <itsaky01@gmail.com>
2022-06-27 10:51:09 +0530

    Added test assertions for PartialReparser

--- lsp/java/src/test/java/com/itsaky/lsp/java/partial/PartialReparserImplTest.kt
+++ lsp/java/src/test/java/com/itsaky/lsp/java/partial/PartialReparserImplTest.kt
@@ -17,11 +17,20 @@
 
 package com.itsaky.lsp.java.partial
 
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import com.itsaky.lsp.java.BaseJavaTest
 import com.itsaky.lsp.java.JavaLanguageServer
 import com.itsaky.lsp.java.visitors.FindMethodAt
+import com.sun.source.tree.ExpressionStatementTree
+import com.sun.source.tree.LiteralTree
 import com.sun.source.tree.MethodTree
+import com.sun.source.tree.Tree
+import com.sun.tools.javac.tree.JCTree.JCCompilationUnit
+import com.sun.tools.javac.tree.JCTree.JCMethodDecl
+import com.sun.tools.javac.tree.JCTree.JCMethodInvocation
+import com.sun.tools.javac.tree.JCTree.JCVariableDecl
+import com.sun.tools.javac.tree.TreeScanner
+import javax.lang.model.type.ArrayType
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
@@ -37,21 +46,92 @@ class PartialReparserImplTest : BaseJavaTest() {
   @Test
   fun parseMethod() {
     openFile("PartialReparserTest")
-    val task =
-      jls.compiler.compile(file).get { task ->
-        val reparser = PartialReparserImpl()
-        val method = FindMethodAt(task.task).scan(task.root(), 133)
-        val reparsed = reparser.parseMethod(
+    jls.compiler.compile(file).run { task ->
+      val reparser = PartialReparserImpl()
+      val method = FindMethodAt(task.task).scan(task.root(), 133)
+      val reparsed =
+        reparser.parseMethod(
           CompilationInfo(task.task, task.diagnosticListener, task.root()),
           method!!.leaf as MethodTree,
           "{System.out.println(\"Hello world!\"); var klass = String.class;}"
         )
-        
-        Truth.assertThat(reparsed).isTrue()
-      }
+
+      assertThat(reparsed).isTrue()
+      AssertingScanner().scan(task.root() as JCCompilationUnit)
+    }
   }
 
   override fun openFile(fileName: String) {
     super.openFile("partial/$fileName")
+  }
+
+  class AssertingScanner : TreeScanner() {
+    private var methodCount = 0
+    override fun visitMethodDef(tree: JCMethodDecl?) {
+      assertThat(tree).isNotNull()
+      tree!!
+
+      if (tree.name.contentEquals("<init>")) {
+        // Javac automatically adds the deafult constructor
+        methodCount++
+        return super.visitMethodDef(tree)
+      }
+
+      assertThat(methodCount).isEqualTo(1)
+      methodCount++
+
+      val params = tree.params
+      assertThat(params).isNotNull()
+      assertThat(params.size).isEqualTo(1)
+
+      val argsParam = params[0]
+      assertThat(argsParam.name.toString()).isEqualTo("args")
+      assertThat(argsParam.type).isInstanceOf(ArrayType::class.java)
+
+      val argType = argsParam.type as ArrayType
+      assertThat(argType.componentType.toString()).isEqualTo("java.lang.String")
+
+      val body = tree.body
+      assertThat(body).isNotNull()
+
+      val statements = body.statements
+      assertThat(statements).isNotNull()
+      assertThat(statements.size).isEqualTo(2)
+
+      val println = statements[0]
+      assertThat(println).isNotNull()
+      assertThat(println.kind).isEqualTo(Tree.Kind.EXPRESSION_STATEMENT)
+      println as ExpressionStatementTree
+
+      val arguments = (println.expression as JCMethodInvocation).arguments
+      assertThat(arguments).isNotNull()
+      assertThat(arguments.size).isEqualTo(1)
+
+      val arg = arguments[0]
+      assertThat(arg).isNotNull()
+      assertThat(arg.kind).isEqualTo(Tree.Kind.STRING_LITERAL)
+      arg as LiteralTree
+      assertThat(arg.value).isEqualTo("Hello world!")
+
+      val varDecl = statements[1]
+      assertThat(varDecl).isNotNull()
+      assertThat(varDecl.kind).isEqualTo(Tree.Kind.VARIABLE)
+      varDecl as JCVariableDecl
+      assertThat(varDecl.name.toString()).isEqualTo("klass")
+
+      val type = varDecl.type
+      assertThat(type).isNotNull()
+      assertThat(type.tsym.qualifiedName.toString()).isEqualTo("java.lang.Class")
+
+      val targs = type.typeArguments
+      assertThat(targs).isNotNull()
+      assertThat(targs.size).isEqualTo(1)
+
+      val tOne = targs[0]
+      assertThat(tOne).isNotNull()
+      assertThat(tOne.tsym.qualifiedName.toString()).isEqualTo("java.lang.String")
+
+      super.visitMethodDef(tree)
+    }
   }
 }
