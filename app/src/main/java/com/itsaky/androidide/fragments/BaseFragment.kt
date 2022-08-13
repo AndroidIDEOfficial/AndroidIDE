6e7a576e69f8e0a086551a8f0518bad000a6a225
Akash Yadav <itsaky01@gmail.com>
2022-08-13 11:48:26 +0530

    Fixed typo

--- app/src/main/java/com/itsaky/androidide/fragments/BaseFragment.kt
+++ app/src/main/java/com/itsaky/androidide/fragments/BaseFragment.kt
@@ -31,11 +31,11 @@ open class BaseFragment : Fragment() {
 
   private var callback: OnDirectoryPickedCallback? = null
   private val allowedAuthorities =
-    setOf(ANDROID_DOCS_AUTHORITY, ANDROIDDIDE_DOCS_AUTHORITY, TERMUX_DOCS_AUTHORITY)
+    setOf(ANDROID_DOCS_AUTHORITY, ANDROIDIDE_DOCS_AUTHORITY, TERMUX_DOCS_AUTHORITY)
 
   companion object {
     const val ANDROID_DOCS_AUTHORITY = "com.android.externalstorage.documents"
-    const val ANDROIDDIDE_DOCS_AUTHORITY = "com.itsaky.androidide.documents"
+    const val ANDROIDIDE_DOCS_AUTHORITY = "com.itsaky.androidide.documents"
     const val TERMUX_DOCS_AUTHORITY = "com.termux.documents"
   }
 
@@ -66,7 +66,7 @@ open class BaseFragment : Fragment() {
       }
 
       val dir =
-        if (authority == ANDROIDDIDE_DOCS_AUTHORITY || authority == TERMUX_DOCS_AUTHORITY) {
+        if (authority == ANDROIDIDE_DOCS_AUTHORITY || authority == TERMUX_DOCS_AUTHORITY) {
           File(docId)
         } else {
           val split = docId.split(':')
