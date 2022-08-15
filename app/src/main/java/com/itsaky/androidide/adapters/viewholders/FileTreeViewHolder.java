4b4be3933e1af98c9d7ecc77d22d4209a00c4c77
Андрей Кожев <andrey.kozhev.2010@outlook.com>
2022-08-15 11:30:22 +0300

    Update

--- app/src/main/java/com/itsaky/androidide/adapters/viewholders/FileTreeViewHolder.java
+++ app/src/main/java/com/itsaky/androidide/adapters/viewholders/FileTreeViewHolder.java
@@ -105,7 +105,7 @@ public class FileTreeViewHolder extends TreeNode.BaseNodeViewHolder<File> {
     else if (file.getName().endsWith(".xml")) icon = R.drawable.ic_language_xml;
     else if (file.getName().endsWith(".gradle")) icon = R.drawable.ic_language_gradle;
     else if (file.getName().endsWith(".json")) icon = R.drawable.ic_language_json;
-    else if (file.getName().endsWith(".json")) icon = R.drawable.ic_language_properties;
+    else if (file.getName().endsWith(".properties")) icon = R.drawable.ic_language_properties;
     else if (ImageUtils.isImage(file)) icon = R.drawable.ic_file_image;
     else icon = R.drawable.ic_file_unknown;
     return icon;
