1131a308d7f1f68661ea566df1e6c57f60f84988
Akash Yadav <itsaky01@gmail.com>
2022-11-06 21:45:18 +0530

    Implemented SpinnerAttrAdapter

---
+++ xml-inflater/src/main/java/com/itsaky/androidide/inflater/internal/adapters/SpinnerAttrAdapter.kt
@@ -0,0 +1,62 @@
+/*
+ *  This file is part of AndroidIDE.
+ *
+ *  AndroidIDE is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  AndroidIDE is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with AndroidIDE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.itsaky.androidide.inflater.internal.adapters
+
+import android.content.Context
+import android.view.ViewGroup.LayoutParams
+import android.widget.Spinner
+import com.itsaky.androidide.annotations.inflater.AttributeAdapter
+import com.itsaky.androidide.inflater.IAttribute
+import com.itsaky.androidide.inflater.INamespace
+import com.itsaky.androidide.inflater.IView
+import com.itsaky.androidide.inflater.internal.LayoutFile
+
+/**
+ * Attribute adapter for [Spinner].
+ *
+ * @author Akash Yadav
+ */
+@AttributeAdapter(Spinner::class)
+class SpinnerAttrAdapter : AbsSpinnerAttrAdapter() {
+  override fun apply(view: IView, attribute: IAttribute): Boolean {
+    return doApply<Spinner>(view, attribute) {
+        _: LayoutFile,
+        context: Context,
+        _: LayoutParams,
+        _: INamespace,
+        name: String,
+        value: String ->
+      var handled = true
+
+      when (name) {
+        "dropDownHorizontalOffset" -> dropDownHorizontalOffset = parseDimension(context, value, 0)
+        "dropDownVerticalOffset" -> dropDownVerticalOffset = parseDimension(context, value, 0)
+        "dropDownWidth" -> dropDownWidth = parseDimension(context, value, 0)
+        "gravity" -> gravity = parseGravity(value)
+        "popupBackground" -> setPopupBackgroundDrawable(parseDrawable(context, value))
+        else -> handled = false
+      }
+
+      if (!handled) {
+        handled = super.apply(view, attribute)
+      }
+
+      return@doApply handled
+    }
+  }
+}
