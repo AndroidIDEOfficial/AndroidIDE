8c8e45aed6fb8709a3f594e0f119ec6d248c947c
Akash Yadav <itsaky01@gmail.com>
2022-11-06 21:02:49 +0530

    Implemented SwitchAttrAdapter

--- xml-inflater/src/main/java/com/itsaky/androidide/inflater/internal/adapters/SwitchAttrAdapter.kt
+++ xml-inflater/src/main/java/com/itsaky/androidide/inflater/internal/adapters/SwitchAttrAdapter.kt
@@ -18,8 +18,9 @@
 package com.itsaky.androidide.inflater.internal.adapters
 
 import android.content.Context
+import android.graphics.Typeface.DEFAULT
 import android.view.ViewGroup.LayoutParams
-import android.widget.CompoundButton
+import android.widget.Switch
 import com.itsaky.androidide.annotations.inflater.AttributeAdapter
 import com.itsaky.androidide.inflater.IAttribute
 import com.itsaky.androidide.inflater.INamespace
@@ -27,27 +28,37 @@ import com.itsaky.androidide.inflater.IView
 import com.itsaky.androidide.inflater.internal.LayoutFile
 
 /**
- * Attribute adapter for [CompoundButton].
+ * Attribute adapter for [Switch].
  *
  * @author Akash Yadav
  */
-@AttributeAdapter(CompoundButton::class)
-abstract class CompoundButtonAttrAdapter : ButtonAttrAdapter() {
-
+@AttributeAdapter(Switch::class)
+class SwitchAttrAdapter : CompoundButtonAttrAdapter() {
   override fun apply(view: IView, attribute: IAttribute): Boolean {
-    return doApply<CompoundButton>(view, attribute) {
-      _: LayoutFile,
-      context: Context,
-      _: LayoutParams,
-      _: INamespace,
-      name: String,
-      value: String ->
+    return doApply<Switch>(view, attribute) {
+        _: LayoutFile,
+        context: Context,
+        _: LayoutParams,
+        _: INamespace,
+        name: String,
+        value: String ->
       var handled = true
-
       when (name) {
-        "buttonTint" -> buttonTintList = parseColorStateList(context, value)
-        "button" -> buttonDrawable = parseDrawable(context, value)
-        "buttonTintMode" -> buttonTintMode = parsePorterDuffMode(value)
+        "showText" -> showText = parseBoolean(value)
+        "splitTrack" -> splitTrack = parseBoolean(value)
+        "switchMinWidth" -> switchMinWidth = parseDimension(context, value, 0)
+        "switchPadding" -> switchPadding = parseDimension(context, value, 0)
+        "textOff" -> textOff = parseString(value)
+        "textOn" -> textOn = parseString(value)
+        "textStyle" -> setSwitchTypeface(DEFAULT, parseTextStyle(value))
+        "thumb" -> thumbDrawable = parseDrawable(context, value)
+        "thumbTextPadding" -> thumbTextPadding = parseDimension(context, value, 0)
+        "thumbTint" -> thumbTintList = parseColorStateList(context, value)
+        "thumbTintMode" -> thumbTintMode = parsePorterDuffMode(value)
+        "track" -> trackDrawable = parseDrawable(context, value)
+        "trackTint" -> trackTintList = parseColorStateList(context, value)
+        "trackTintMode" -> trackTintMode = parsePorterDuffMode(value)
+        "typeface" -> setSwitchTypeface(parseTypeface(value))
         else -> handled = false
       }
 
@@ -57,9 +68,5 @@ abstract class CompoundButtonAttrAdapter : ButtonAttrAdapter() {
 
       return@doApply handled
     }
-  }
-
-  override fun applyBasic(view: IView) {
-    super.applyBasic(view)
   }
 }
