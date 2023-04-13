b85d4cbb7124c9bc9c9c85f15738fded71db70ce
Akash Yadav <itsaky01@gmail.com>
2023-04-14 00:39:02 +0530

    fix(build-info): generate BuildInfo in build directory

--- build-info/build.gradle.kts
+++ build-info/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.incremental.createDirectory
+
 /*
  *  This file is part of AndroidIDE.
  *
@@ -20,9 +22,14 @@ plugins {
   id("com.vanniktech.maven.publish.base")
 }
 
+val buildInfoGenDir = project.buildDir.resolve("generated/buildInfo").also { it.createDirectory() }
+
+sourceSets { getByName("main").java.srcDir(buildInfoGenDir) }
+
 tasks.create("generateBuildInfo") {
-  val buildInfo = project.file("src/main/java/com/itsaky/androidide/buildinfo/BuildInfo.java")
-  val buildInfoIn = project.file("${buildInfo.path}.in")
+  val buildInfoPath = "com/itsaky/androidide/buildinfo/BuildInfo.java"
+  val buildInfo = buildInfoGenDir.resolve(buildInfoPath)
+  val buildInfoIn = project.file("src/main/java/${buildInfoPath}.in")
 
   inputs.file(buildInfoIn)
   outputs.file(buildInfo)
@@ -31,27 +38,23 @@ tasks.create("generateBuildInfo") {
     buildInfoIn.replaceContents(
       dest = buildInfo,
       comment = "//",
-      candidates = arrayOf(
-        "MVN_GROUP_ID" to BuildConfig.packageName,
-
-        "VERSION_NAME" to rootProject.version.toString(),
-        "VERSION_NAME_SIMPLE" to rootProject.simpleVersionName,
-        "VERSION_NAME_PUBLISHING" to rootProject.publishingVersion,
-
-        "CI_BUILD" to CI.isCiBuild.toString(),
-        "CI_GIT_BRANCH" to CI.branchName,
-        "CI_COMMIT_HASH" to CI.commitHash,
-
-        "REPO_HOST" to ProjectConfig.REPO_HOST,
-        "REPO_OWNER" to ProjectConfig.REPO_OWNER,
-        "REPO_NAME" to ProjectConfig.REPO_NAME,
-
-        "PROJECT_SITE" to ProjectConfig.PROJECT_SITE,
-      )
+      candidates =
+        arrayOf(
+          "MVN_GROUP_ID" to BuildConfig.packageName,
+          "VERSION_NAME" to rootProject.version.toString(),
+          "VERSION_NAME_SIMPLE" to rootProject.simpleVersionName,
+          "VERSION_NAME_PUBLISHING" to rootProject.publishingVersion,
+          "CI_BUILD" to CI.isCiBuild.toString(),
+          "CI_GIT_BRANCH" to CI.branchName,
+          "CI_COMMIT_HASH" to CI.commitHash,
+          "REPO_HOST" to ProjectConfig.REPO_HOST,
+          "REPO_OWNER" to ProjectConfig.REPO_OWNER,
+          "REPO_NAME" to ProjectConfig.REPO_NAME,
+          "PROJECT_SITE" to ProjectConfig.PROJECT_SITE,
+        )
     )
   }
 }
 
-tasks.withType<JavaCompile> {
-  dependsOn("generateBuildInfo")
-}
+tasks.withType<JavaCompile> { dependsOn("generateBuildInfo") }
+tasks.withType<Jar> { dependsOn("generateBuildInfo") }
