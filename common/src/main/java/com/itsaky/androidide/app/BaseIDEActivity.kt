9f487dd7af544423b5fb3a0b43299515b6107b95
Vivek <itsvks19@gmail.com>
2023-12-09 14:53:37 +0530

    chore: remove unused imports

--- common/src/main/java/com/itsaky/androidide/app/BaseIDEActivity.kt
+++ common/src/main/java/com/itsaky/androidide/app/BaseIDEActivity.kt
@@ -16,13 +16,9 @@
  */
 package com.itsaky.androidide.app
 
-import android.Manifest.permission
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.itsaky.androidide.common.R
 import com.itsaky.androidide.tasks.cancelIfActive
