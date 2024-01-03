e285f7ffb97c9afda4593a5b66f7013fb4e8bcdf
Akash Yadav <itsaky01@gmail.com>
2024-01-03 08:32:26 +0530

    build: use republished Grade tooling API (#545)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -43,8 +43,8 @@ dependencyResolutionManagement {
     google()
     mavenCentral()
     maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
+    maven { url = uri("https://s01.oss.sonatype.org/content/groups/public/") }
     maven { url = uri("https://jitpack.io") }
-    maven { url = uri("https://repo.gradle.org/gradle/libs-releases/") }
   }
 }
 
