a9f7c354f5bfb8d35f3be89cecbd3b35cf8231ae
Akash Yadav <itsaky01@gmail.com>
2024-05-29 21:36:12 +0530

    build: download signing key before build

--- composite-builds/build-logic/plugins/src/main/java/com/itsaky/androidide/plugins/SigningConfigPlugin.kt
+++ composite-builds/build-logic/plugins/src/main/java/com/itsaky/androidide/plugins/SigningConfigPlugin.kt
@@ -17,16 +17,16 @@
 
 package com.itsaky.androidide.plugins
 
+import com.android.build.gradle.BaseExtension
 import com.itsaky.androidide.build.config.KEY_ALIAS
 import com.itsaky.androidide.build.config.KEY_PASS
 import com.itsaky.androidide.build.config.KEY_STORE_PASS
-import com.android.build.gradle.BaseExtension
+import com.itsaky.androidide.build.config.isFDroidBuild
+import com.itsaky.androidide.build.config.signingKey
 import com.itsaky.androidide.plugins.util.SigningKeyUtils.downloadSigningKey
 import com.itsaky.androidide.plugins.util.SigningKeyUtils.getEnvOrProp
-import com.itsaky.androidide.build.config.isFDroidBuild
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import com.itsaky.androidide.build.config.signingKey
 
 /**
  * Configures the signing keys to application modules.
@@ -42,6 +42,8 @@ class SigningConfigPlugin : Plugin<Project> {
         logger.warn("!!! Do not apply ${javaClass.simpleName} when building or F-Droid.")
         return
       }
+
+      downloadSigningKey()
 
       val signingKey = signingKey.get().asFile
       if (!signingKey.exists()) {
